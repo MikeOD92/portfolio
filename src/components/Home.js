5c58df7b244ad45d553491998019787a05f9f4cc
Michael <michaelodell660@gmail.com>
2022-03-06 19:19:27 -0800

    added react-spring and made fade out animation better

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,69 @@
-import React, {useState, useEffect} from 'react';
-
-import {Container,Row, Col} from 'react-bootstrap';
-import Skills from './Skills'
-import Portfolio from './Portfolio'
-import About from './About'
+import React, { useState, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
+import { Container, Row, Col } from "react-bootstrap";
+import Skills from "./Skills";
+import Portfolio from "./Portfolio";
+import About from "./About";
 
 export default function Home() {
+  const [show, setShow] = useState(true);
 
-  const [show, setShow] = useState(true)
+  const animate = useSpring({
+    opacity: show ? 1 : 0,
+    // scale: show ? 1 : 0,
+    config: { duration: 50 },
+  });
 
   const controlSplash = () => {
-    if(window.scrollY > window.visualViewport.height){
-      setShow(false)
-    }else setShow(true)
-  } 
+    if (window.scrollY > window.visualViewport.height - 100) {
+      setShow(false);
+    } else setShow(true);
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', controlSplash)
+    window.addEventListener("scroll", controlSplash);
     return () => {
-      window.removeEventListener('scroll', controlSplash)
-    }
-  }, [])
-  
-  return (
-    <Container  fluid style={{paddingLeft: "0", paddingRight: "0"}}>
+      window.removeEventListener("scroll", controlSplash);
+    };
+  }, []);
 
-        <Container fluid className={`splash ${show && 'splash__visable'}`} style={{paddingLeft: "0", paddingRight: "0"}}> {/*animate__animated animate__fadeOut animate__delay-5s*/}
-              <Row>
-                <Col>
-                  <h1 className='splashTitle' > _Michael_ T_ <br/>_O'Dell </h1>
-                </Col>
-                <Col>
-                  <h2 className="splashSubTitle"> Software Engineer~  <br/>Web Developer~ <br/> Artist~</h2>
-                </Col>
-              </Row>
-        </Container>
+  return (
+    <Container fluid style={{ paddingLeft: "0", paddingRight: "0" }}>
+      <animated.div
+        fluid
+        className={`splash ${show && "splash__visable"}`}
+        style={animate}
+      >
+        {" "}
+        <Row>
+          <Col>
+            <h1 className="splashTitle">
+              {" "}
+              _Michael_ T_ <br />
+              _O'Dell{" "}
+            </h1>
+          </Col>
+          <Col>
+            <h2 className="splashSubTitle">
+              {" "}
+              Software Engineer~ <br />
+              Web Developer~ <br /> Artist~
+            </h2>
+          </Col>
+        </Row>
+      </animated.div>
 
       <Container fluid>
         <Row id="about">
-          <About/>
+          <About />
         </Row>
         <Row>
-          <Skills/>
+          <Skills />
         </Row>
         <Row>
-          <Portfolio/>
+          <Portfolio />
         </Row>
       </Container>
     </Container>
-  )
+  );
 }
